2575a5d6b958cff47e6a3480d901671f6a69016c
jawad0098 <hassanjawad858@gmail.com>
2024-11-21 14:21:42 +0500

    contact-page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { CiFacebook } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaSquareGithub } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
+
 
 const Contact = () => {
   return (
     <>
       <div className="conatiner contact">
-        <h1>CONTACT</h1>
+        <h1>CONTACT ME</h1>
         <div className="contact-icon">
-            <div className="items"></div>
-            <div className="items"></div>
-            <div className="items"></div>
-            <div className="items"></div>
-            <div className="items"></div>
+            <a href='https://www.instagram.com' target='_blank' className="items"><FaInstagram className='icons'  /></a>
+            <a href='https://www.facebook.com' target='_blank' className="items"><CiFacebook className='icons' /></a>
+            <a href='https://www.linkedin.com' target='_blank' className="items"><CiLinkedin className='icons' /></a>
+            <a href='https://www.twitter.com' target='_blank' className="items"><FaSquareXTwitter className='icons' /></a>
+            <a href='https://www.github.com' target='_blank' className="items"><FaSquareGithub className='icons' /></a>
+            <a href="mailto:" target='_blank'  className="items"><SiGmail className='icons' /></a>
+          
         </div>
       </div>
     </>
